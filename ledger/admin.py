22dcae69890e6749a6d46d86d76f1dcf577c6db9
bhattaraiabinash <bhattaraiavinash4@gmail.com>
2026-07-13 09:13:39 +0545

    feat: register Phase 3 models in Django admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Vendor, Invoice, InvoiceLineItem
+from .models import (
+    
+Vendor, Invoice, InvoiceLineItem, Product, VendorProductPrice,
+ReconciliationResult
 
+)
 admin.site.register(Vendor)
 admin.site.register(Invoice)
 admin.site.register(InvoiceLineItem)
+admin.site.register(Product)
+admin.site.register(VendorProductPrice)
+admin.site.register(ReconciliationResult)
